e131bca4704f7067df967abd3077f8b2b2a64ab6
venkatchand <vcvenkatrocks@gmail.com>
2016-12-26 18:54:37 +0530

    Added some codes

---
+++ JavaTest/src/com/java/test/testMe.java
@@ -0,0 +1,17 @@
+package com.java.test;
+
+public class testMe {
+    public static void main (String [] args) 
+    {
+        Object o = new Object() /* Line 5 */
+        {
+            public boolean equals(Object obj) 
+            {
+                return true;
+            } 
+        };      /* Line 11 */
+        
+        System.out.println(o.equals("Fred"));
+    }
+
+}
